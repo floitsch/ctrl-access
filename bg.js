42579914920829fb580c8d5ae7dedadcc589aef8
Florian Loitsch <florian@loitsch.com>
2012-07-29 21:17:34 +0200

    Address comment.
    Remove unused t variable.

--- bg.js
+++ bg.js
@@ -1,6 +1,5 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.method == "getLocalStoragePrefs") {
-    var t = localStorage['prefs'];
     sendResponse({prefs: localStorage['prefs']});
   } else
     sendResponse({});  // Nothing to see.
